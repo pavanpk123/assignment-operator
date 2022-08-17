84a63e58a2650037f5dbdb8c601dad7017a63d03
pavan <Pavanpk123@gmail.com>
2022-08-17 15:27:09 +0530

    assignment operator

---
+++ assignment.js
@@ -0,0 +1,24 @@
+var x = 10;
+var  y = 16;
+var z = (x+=y);
+console.log(z);
+
+var x = 10;
+var  y = 16;
+var z = (x-=y);
+console.log(z);
+
+var x = 10;
+var  y = 16;
+var z = (x*=y);
+console.log(z);
+
+var x = 10;
+var  y = 16;
+var z = (x/=y);
+console.log(z);
+
+var x = 10;
+var  y = 16;
+var z = (x%=y);
+console.log(z);
